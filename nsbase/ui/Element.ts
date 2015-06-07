a87ee0e919c48913275b4778331913afdf1af046
Pete <kungfusheep@gmail.com>
2015-06-07 13:36:45 +0100

    started building ui framework.

---
+++ nsbase/ui/Element.ts
@@ -0,0 +1,22 @@
+
+module nsbase_ui {
+	
+	
+	export class Element {
+		
+		public initialized:boolean;
+		
+		
+		
+		constructor(){
+			
+		}
+		
+		
+		
+		public initialize() : void {
+			
+		}
+	}
+	
+}
